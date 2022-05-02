8c84883994b31b0195b39435d26a8ee10c89c84b
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-02 17:55:12 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+double maggiore(double x[], int d)
+{
+    double m=x[1];
+    for(int i=0;i<d;i++)
+    {
+        if(x[i]>m)
+        {
+            m=x[i];
+        }
+    }
+    return m;
+}
+
+
+
 
